8e574aba735256893caf4997c41f6462c87a0dca
H3ll3m4 <26165112+H3ll3m4@users.noreply.github.com>
2019-04-11 13:49:05 +0100

    Add files via upload

---
+++ rpmControl.cpp
@@ -0,0 +1,33 @@
+#include "rpmControl.h"
+
+rpmControl::rpmControl() {
+	pinMode(_pinLED, OUTPUT);
+	pinMode(_pinPWM, OUTPUT);
+	pinMode(_pinPotentiometer, INPUT);
+  pinMode(_pinSpeed, INPUT);
+
+}
+
+// Counting pulse using interrupts
+ void rpmControl::readSpeed(){
+      if (_half_revolutions >= _resolution) { 
+     _rpm = 30*1000/(millis() - _timeold)*_half_revolutions;
+     _timeold = millis();
+     _half_revolutions = 0;
+   }
+  }
+
+ void rpmControl::setRPMSpeed(){
+  int wantedSpeed = analogRead(_pinPotentiometer);
+  //DutyCyle for the PWM : from 0 to 255 (full Speedintint)
+  int dutyCycle = map(wantedSpeed, 0, 1023, 0, 1023); // map to the range of the analog output
+  int timerus = 1000000 ; 
+  CurieTimerOne.pwmStart(_pinPWM,dutyCycle,timerus);
+  }
+
+ void rpmControl::rpm_fun()
+ {
+   _half_revolutions++;
+   //Each rotation, this interrupt function is run twice
+ }
+
---
+++ rpmControl.h
@@ -0,0 +1,34 @@
+// rpmControl.h
+
+#ifndef _rpmCONTROL_h
+#define _rpmCONTROL_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include "CurieTimerOne.h"
+
+class rpmControl {
+public: 
+	rpmControl();
+  void readSpeed();
+  void setRPMSpeed();
+
+
+private:
+	int _pinPotentiometer = A0;
+	int _pinPWM = 9;
+	int _pinLED = 7;
+	int _pinSpeed = 3; 
+  volatile byte _half_revolutions;
+  unsigned int _rpm;
+  unsigned long _timeold;
+  int _resolution = 40;// Resolution: how often update RPM, use 30. Can increase for better resolution or decrease for faster result
+  void rpm_fun();
+};
+
+#endif
+
